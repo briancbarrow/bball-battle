dd059026464b99c3593e106d94ceb47b5d953943
Brian Barrow <briancbarrow@gmail.com>
2016-12-21 05:34:16 -0700

    adding travis CI

---
+++ o-rdoc/travis.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby22-x64\bin\ruby.exe" "C:/git/thinkful-projects/bball-battle/o-rdoc/travis" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby22-x64\bin\ruby.exe" "%~dpn0" %*
