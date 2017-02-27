441a29ca938a45f6247763bfc982fe11bb3c8ea7
Brian Barrow <briancbarrow@gmail.com>
2017-02-26 20:18:48 -0700

    fixed footer

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <footer>
-          <p className="footer-heading">Created By <a target="_blank" href="https://www.briancbarrow.com">Brian Barrow</a></p>
+          <p className="footer-heading">Created by <a target="_blank" href="https://www.briancbarrow.com">Brian Barrow</a></p>
           <a className="contact-link" target="_blank" href="https://github.com/briancbarrow"><FontAwesome name="github" /></a>
           <a className="contact-link" target="_blank" href="https://www.linkedin.com/in/barrowbrian"><FontAwesome name="linkedin" /></a>
           <a className="contact-link" target="_blank" href="https://twitter.com/the_BrianB"><FontAwesome name="twitter" /></a>
